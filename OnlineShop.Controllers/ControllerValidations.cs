ede42219ae187d811c52d7c8c17c58478e861e19
mkovachev <mkovachev6@gmail.com>
2019-11-11 20:29:12 +0200

    created

---
+++ OnlineShop.Controllers/ControllerValidations.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Controllers
+{
+    public static class ControllerValidations
+    {
+        public const string AdministratorRole = "Administrator";
+        public const string AdministratorArea = "Admin";
+        public const string SuccessMessage = "SuccessMessage";
+    }
+}
