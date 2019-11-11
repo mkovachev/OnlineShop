aaada759ed0be72255aae2d814992583f0e3f7c3
mkovachev <mkovachev6@gmail.com>
2019-11-11 20:30:22 +0200

    created admin

--- OnlineShop.Web/Infrastructure/ApplicationBuilderExtensions.cs
+++ OnlineShop.Web/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OnlineShop.Controllers;
+using System.Threading.Tasks;
 
 namespace OnlineShop.Web.Infrastructure
 {
@@ -31,5 +37,54 @@ namespace OnlineShop.Web.Infrastructure
                             pattern: "{controller=Home}/{action=Index}/{id?}");
                         endpoints.MapRazorPages();
                     });
+
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                var dbContext = services.GetService<OnlineShopDbContext>();
+
+                await dbContext.Database.MigrateAsync();
+
+                var roleManager = services.GetService<RoleManager<IdentityRole>>();
+                var existingRole = await roleManager.FindByNameAsync(ControllerValidations.AdministratorRole);
+                if (existingRole != null)
+                {
+                    return app;
+                }
+
+                var adminRole = new IdentityRole(ControllerValidations.AdministratorRole);
+
+                await roleManager.CreateAsync(adminRole);
+
+                // create user
+                //var adminUser = new User
+                //{
+                //    UserName = "admin@onlineshop.com",
+                //    Email = "admin@onlineshop.com",
+                //    SecurityStamp = "AdminsecurityStamp"
+                //};
+
+                //var userManager = services.GetService<UserManager<User>>();
+
+                //await userManager.CreateAsync(adminUser, "adminpass");
+
+                //await userManager.AddToRoleAsync(adminUser, ControllerValidations.AdministratorRole);
+
+                //var user = new User
+                //{
+                //    UserName = "normal@onlineshop.com",
+                //    Email = "normal@onlineshop.com",
+                //    SecurityStamp = "UserSecurityStamp"
+                //};
+
+                //await userManager.CreateAsync(user, "userpass");
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return app;
+        }
     }
 }
