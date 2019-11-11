9e5d160e3a00aeb6406363e3980412b74e595703
mkovachev <mkovachev6@gmail.com>
2019-11-11 20:09:16 +0200

    created

---
+++ OnlineShop.Data/Models/AttributeParam.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Data.Models
+{
+    public partial class AttributeParam
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ProductAttributeId { get; set; }
+
+        public virtual ProductAttribute ProductAttribute { get; set; }
+    }
+}
