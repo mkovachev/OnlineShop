991071e5d8e5f361c6c3ee4af3b2cd45fb85bfac
mkovachev <mkovachev6@gmail.com>
2019-11-11 18:09:47 +0200

    created

---
+++ OnlineShop.Data/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(DataConstants.ProductTitleMinLength)]
+        [MaxLength(DataConstants.ProductTitleMaxLength)]
+        public string Title { get; set; }
+
+        [MaxLength(DataConstants.ProductShortDescriptionMaxLength)]
+        public string ShortDescription { get; set; }
+
+        [MinLength(DataConstants.ProductDescriptionMinLength)]
+        [MaxLength(DataConstants.ProductDescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Range(0, 10000)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(2000)]
+        [DataType(DataType.ImageUrl)]
+        public string Thumbnail { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.mm.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateCreated { get; set; }
+
+        public ICollection<Image> Images { get; set; } = new List<Image>();
+
+        public ICollection<ProductAttribute> ProductAttributes { get; set; } = new List<ProductAttribute>();
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
