daa303d40dcb2da1e18f6f2ea868660907242e65
mkovachev <mkovachev6@gmail.com>
2019-11-11 20:08:24 +0200

    created

---
+++ OnlineShop.Data/Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Data.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(4)]
+        public string FileExtension { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ OnlineShop.Data/Models/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Data.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        [Range(0, 10000)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public int OrderId { get; set; }
+
+        public virtual Order Order { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
