01a006ee1f59c0dd235204375ca0f7e412ef4696
mkovachev <mkovachev6@gmail.com>
2019-11-20 16:54:26 +0200

    added

---
+++ OnlineShop.Services/Infrastructure/ServiceMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OnlineShop.Data.Models;
+using OnlineShop.Services.Admin.Models;
+
+namespace OnlineShop.Services.Infrastructure
+{
+    public class ServiceMappingProfile : Profile
+    {
+        public ServiceMappingProfile()
+        {
+            this.CreateMap<Category, AdminCategoryServiceModel>();
+                //.ForMember(a => a.Name, opt => opt.MapFrom(a => a.Name));
+
+           
+        }
+    }
