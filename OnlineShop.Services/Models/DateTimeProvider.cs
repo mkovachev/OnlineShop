65769646bfee9e1d46df640e1a94c97b9b8096f6
mkovachev <mkovachev6@gmail.com>
2019-11-20 16:35:43 +0200

    created

---
+++ OnlineShop.Services/Models/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Services.Interfaces;
+using System;
+
+namespace OnlineShop.Services.Models
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow() => DateTime.UtcNow;
+    }
+}
