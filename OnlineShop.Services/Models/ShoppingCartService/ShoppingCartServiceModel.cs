e4f8020090774468d30154eba2e6deb0c81dc076
mkovachev <mkovachev6@gmail.com>
2019-11-30 02:02:31 +0200

    added mapping

--- OnlineShop.Services/Models/ShoppingCartService/ShoppingCartServiceModel.cs
+++ OnlineShop.Services/Models/ShoppingCartService/ShoppingCartServiceModel.cs
@@ -1,6 +1,8 @@
-﻿namespace OnlineShop.Services.Models.ShoppingCartService
+﻿using OnlineShop.Common.Mapping;
+
+namespace OnlineShop.Services.Models.ShoppingCartService
 {
-    public class ShoppingCartServiceModel
+    public class ShoppingCartServiceModel : IMapFrom<ShoppingCart>
     {
         public string Id { get; set; }
     }
