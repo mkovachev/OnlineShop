53e9559f304352c463a8c9d150f80415d2994a80
mkovachev <mkovachev6@gmail.com>
2019-12-30 13:24:38 +0200

    added

---
+++ OnlineShop.Services/Implementations/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using Data;
+using OnlineShop.Data.Models;
+using OnlineShop.Services.Interfaces;
+using OnlineShop.Services.Models.ShoppingCartService;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly OnlineShopDbContext db;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderService(OnlineShopDbContext db, ShoppingCart shoppingCart)
+        {
+            this.db = db;
+            this.shoppingCart = shoppingCart;
+        }
+
+        public async Task CreateOrderAsync(Order order)
+        {
+            await this.db.Orders.AddAsync(order);
+
+            var shoppingCartItems = this.shoppingCart.ShoppingCartItems;
+
+            foreach (var product in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    //Amount = product.Amount,
+                    //ProductId = product.Product.Id,
+                    //Price = product.Product.Price,
+                    OrderId = order.Id,
+                    OrderPlaced = DateTime.UtcNow
+            };
+
+                await this.db.OrderDetails.AddAsync(orderDetail);
+
+                await this.db.SaveChangesAsync();
+            }
+
+        }
+    }
+}
